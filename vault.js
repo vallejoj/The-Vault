9467d893f4d4eedc8309971c00fcdae64311bc82
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-18 19:44:43 -1000

    UGGGGGGGGGG

--- vault.js
+++ vault.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = function() {
-  
+  var daKey;
+  var daValue;
+    function setValue(key, value){
+    daKey=key
+    daValue=value
+}
+    function getValue(key){
+if (daKeys) {
+  return daValue
+}
+    }
+    return{key:key,
+    value:value}
 };
